eca68018650c1dfa7b1b3a2578f45bd766659fae
AliakseiS97 <aliakseis97@gmail.com>
2026-05-12 19:23:40 +0200

    Add Spring Security with roles, UserDetails, SecurityFilterChain and PreAuthorize

--- src/main/java/mate/academy/service/UserServiceImpl.java
+++ src/main/java/mate/academy/service/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package mate.academy.service;
 
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import mate.academy.dto.request.UserRegistrationRequestDto;
 import mate.academy.dto.response.UserResponseDto;
 import mate.academy.exception.RegistrationException;
 import mate.academy.mapper.UserMapper;
+import mate.academy.model.Role;
 import mate.academy.model.User;
+import mate.academy.repository.RoleRepository;
 import mate.academy.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(
@@ -25,6 +29,9 @@ public class UserServiceImpl implements UserService {
         }
         User user = userMapper.toModel(requestDto);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        Role userRole = roleRepository.findByName(Role.RoleName.ROLE_USER)
+                .orElseThrow(() -> new RegistrationException("Role not found"));
+        user.setRoles(Set.of(userRole));
         User savedUser = userRepository.save(user);
         return userMapper.toDto(savedUser);
     }
